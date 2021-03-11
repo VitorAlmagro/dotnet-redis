152b2db25fb1075fbe2ada257be0bd9cd330250f
Vitor Almagro <vitor_almagro@hotmail.com>
2021-03-10 23:57:02 -0300

    commit inicial

---
+++ ApiRedis/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ApiRedis.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly IDistributedCache _cache;
+
+        public HomeController(IDistributedCache cache)
+        {
+            _cache = cache;
+        }
+
+        [HttpGet]
+        [Route("redis")]
+        public async Task<IActionResult> Get()
+        {
+            var stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+
+            string valorJSON = _cache.GetString("KeyExample");
+
+            if (valorJSON == null)
+            {
+                // Simulando busca informação do banco
+                await Task.Delay(5000);
+                valorJSON = "example";
+
+                DistributedCacheEntryOptions opcoesCache =
+                          new DistributedCacheEntryOptions();
+                opcoesCache.SetAbsoluteExpiration(
+                    TimeSpan.FromSeconds(30));
+
+                _cache.SetString("KeyExample", valorJSON, opcoesCache);
+            }
+
+            stopwatch.Stop();
+
+            return Ok(new 
+            {
+                tempo = stopwatch.ElapsedMilliseconds,
+                valor = valorJSON
+            });
+        }
+    }
+}
---
+++ ApiRedis/Settings.cs
@@ -0,0 +1,7 @@
+﻿namespace ApiRedis
+{
+    public static class Settings
+    {
+        public static string ConnectionRedis = "localhost:6379,password=ApiRedis";
+    }
+}
